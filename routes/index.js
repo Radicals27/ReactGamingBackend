39f96820f41780f903ccbcdb46f0dde121b75c23
Radicals <cclose27@gmail.com>
2020-07-24 11:34:22 +1000

    nodemon running no errors

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,22 @@ var express = require('express')
 var router = express.Router()
 var axios = require('axios')
 
+// Require Statements
+const mongoose = require('mongoose')
+
+// for cross site communication
+const cors = require('cors')
+
+const passport = require("passport")
+// uses the cookies to read session data 
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session);
+const bodyParser = require('body-parser')
+const {User} = require('../models/user')
+
+require('./passport')
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   axios.get(`https://api.rawg.io/api/games`)
