4ff7810b1daad10e0328714bc63c3856805fd20f
Radicals <cclose27@gmail.com>
2020-07-28 09:44:51 +1000

    added Conrads  oauth credentials to passport

--- routes/passport.js
+++ routes/passport.js
@@ -1,48 +1,48 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-const {User, OauthUser} = require('../models/user')
+const { User, OauthUser } = require('../models/user')
 
 // Hash the user password
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     // this method is done( no error, here is the user)
     done(null, user)
 })
 
 // Un-hash the user password back into a string
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
     // this method is done( no error, here is the user)
     done(null, user)
 })
 
 // Initializing a Local Strategy on the User Model
-passport.use(User.createStrategy());
+passport.use(User.createStrategy())
 
 // Create a google strategy, import package
 passport.use(new GoogleStrategy({
-    clientID: "876353669737-s4f71ldd024ckdpp4d45j6ovu4v5b0as.apps.googleusercontent.com",
-    clientSecret: "ofmSd8kYqq5_v83dEajZg-OL",
+    clientID: "157895230249-agrvo2b25jecn7isgs81hsv77khm7lfs.apps.googleusercontent.com",
+    clientSecret: "QA8lxUiDJaxm6y4FeWotV73l",
     callbackURL: "https://react-gaming-backend.herokuapp.com/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    OauthUser.findOrCreate({id: profile.id, displayName: profile.displayName}, 'googleId', function(err, user) {
-        return cb(err, user);
-    });
-  })
+},
+    function (accessToken, refreshToken, profile, cb) {
+        OauthUser.findOrCreate({ id: profile.id, displayName: profile.displayName }, 'googleId', function (err, user) {
+            return cb(err, user)
+        })
+    })
 )
 
 // Set up Discord Strategy
-var DiscordStrategy = require('passport-discord').Strategy;
- 
-var scopes = ['identify', 'email'];
- 
+var DiscordStrategy = require('passport-discord').Strategy
+
+var scopes = ['identify', 'email']
+
 passport.use(new DiscordStrategy({
-    clientID: '734966453014626315',
-    clientSecret: 'dvukCLIrX4yQrbDfUsFLt7oSNlr77X9t',
+    clientID: '737455304077213696',
+    clientSecret: 'Xo1CmGRKZv38MXUCwm7u9p-rdM0eweQk',
     callbackURL: 'https://react-gaming-backend.herokuapp.com/auth/discord/callback',
     scope: scopes
 },
-function(accessToken, refreshToken, profile, cb) {
-    OauthUser.findOrCreate({id: profile.id, displayName: profile.username}, 'discordId', function(err, user) {
-        return cb(err, user);
-    });
-}));
+    function (accessToken, refreshToken, profile, cb) {
+        OauthUser.findOrCreate({ id: profile.id, displayName: profile.username }, 'discordId', function (err, user) {
+            return cb(err, user)
+        })
+    }))
