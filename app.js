09609330fd59a5fb58e483682a1bdcf235356eba
Radicals <cclose27@gmail.com>
2020-07-30 09:15:20 +1000

    fixing login issue

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.post('/users/register', (req, res) => {
 
 // If page refreshes, get current user
 app.get('/users/me', (req, res) => {
-  console.log(`user being sent: ${req.user}`)
+  console.log(`user being sent: ${JSON.stringify(req)}`)
   res.send(req.user)
 })
 
